b720b66454f44405839aab2cb7e952821837a10f
olesyamaniuk <maniukolesya222@gmail.com>
2025-03-02 17:41:07 +0200

    2

--- js/slick.js
+++ js/slick.js
@@ -9,7 +9,7 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: false,
+    dots: true,
     centerMode: true,
     cssEase: 'linear',
     prevArrow: '.gallery-list-left',
@@ -37,6 +37,22 @@ $(function () {
 
 
 
+$(function () {
+  $('.features-list').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: true,
+    centerMode: true,
+    cssEase: 'linear',
+    prevArrow: '.features-list-left',
+    nextArrow: '.features-list-right',
+  });
+});
 
 
 
